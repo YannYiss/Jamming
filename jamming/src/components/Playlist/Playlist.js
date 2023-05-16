68b799c6c2190e60d48b5db4482766c6ab744eef
Adrian Nieto <adrian_nc@outlook.com>
2023-05-16 17:26:16 -0600

    Basic layout applied to app

--- jamming/src/components/Playlist/Playlist.js
+++ jamming/src/components/Playlist/Playlist.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Button from '../Button/Button'
 
 export default function Playlist() {
   return (
+    <>
     <div>Playlist</div>
+    <Button/>
+    </>
   )
 }
--- jamming/src/components/SearchBar/SearchBar.js
+++ jamming/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Button from '../Button/Button'
 
 export default function SearchBar() {
   return (
+    <>
     <div>SearchBar</div>
+    <Button />
+    </>
   )
 }
