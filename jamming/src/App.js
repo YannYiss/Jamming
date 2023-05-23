7a1472d23973927fc470afd16f814a02912e6c7a
Adrian Nieto <adrian_nc@outlook.com>
2023-05-23 12:05:31 -0600

    optimized and debugged add and remove functions

--- jamming/src/App.js
+++ jamming/src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [songList, setSongList] = useState([]) 
   const [chosenSongs, setChosenSongs] = useState([]);
   const [playlistName, setPlaylistName] = useState(['New Playlist']);
+  const [playlist, setPlaylist] = useState([]);
 
   const searchSongs = useCallback((term) => {
     Spotify.search(term).then(setSongList);
@@ -16,51 +17,50 @@ function App() {
 
   const addTrack = (e) => {
     const selectedSong = e.target.parentElement.children[0];
+    const songURI = e.target.parentElement.id;
     const songAdded = {
       songName: selectedSong.querySelector('h3').innerText,
       artist: selectedSong.querySelector('h4').innerText,
       album: selectedSong.querySelector('h5').innerText,
-      uri: e.target.parentElement.id
+      uri: songURI
     }
-    setChosenSongs(songList => [...songList,songAdded]);
+    if(playlist.includes(songURI)) {
+      alert('This song was already added to the playlist');
+    } else {
+      setPlaylist([...playlist, songURI]);
+      setChosenSongs(songList => [...songList,songAdded]);
+    }
   };
 
   const removeTrack = (e) => {
     const selectedSong = e.target.parentElement.children[0];
+    const songURI = e.target.parentElement.id;
     const songToRemove = {
       songName: selectedSong.querySelector('h3').innerText,
       artist: selectedSong.querySelector('h4').innerText,
       album: selectedSong.querySelector('h5').innerText,
-      uri: e.target.parentElement.id
+      uri: songURI
     };
-    setChosenSongs(songList => {
-      const updatedArray = songList.filter((song) => {
-        if (song.uri === songToRemove.uri) {
-          return false
-        } else {
-          return true 
-        }
-      });
-      return updatedArray
+    const updatedAray = chosenSongs.filter(song => {
+      if (song.uri === songToRemove.uri) {
+        return false
+      } else {
+        return true 
+      }
     });
+    setChosenSongs(updatedAray);
+    setPlaylist(updatedAray);
   };
 
   const renamePlaylist = (e) => {
     setPlaylistName(e.target.value);
   };
 
-  const savePlaylist = () => {
-    let songsURI = [];
-    chosenSongs.map(song => songsURI.push(song.uri));
-    console.log(songsURI);
-    return songsURI;
-  }
-
   return (
     <div className="App">
       <SearchBar onSearch={searchSongs}/>
       <SearchResults songList={songList} clickHandler={addTrack}/>
-      <Playlist chosenSongs={chosenSongs} clickHandler={removeTrack} typeHandler={renamePlaylist} submitHandler={savePlaylist} playlistName={playlistName}/>
+      <Playlist chosenSongs={chosenSongs} clickHandler={removeTrack} typeHandler={renamePlaylist} playlistName={playlistName}/>
     </div>
   );
 }
